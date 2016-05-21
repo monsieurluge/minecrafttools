356ea6e02fd6b3f2c55e6ac37ee668a1d66ee3e7
MonsieurLuge <monsieurluge@gmail.com>
2016-05-21 18:24:43 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from codecs     import open
+from os         import path
+from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
+here = path.abspath(path.dirname(__file__))
 
-with open('LICENSE') as f:
-    license = f.read()
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
+    readmeContent = readme.read()
+
+with open(path.join(here, 'LICENSE.txt'), encoding='utf-8') as license:
+    licenseContent = license.read()
 
 setup(
     name                = 'minecrafttools',
     version             = '0.1.0',
-    description         = 'Minecraft Tools Library',
-    long_description    = readme,
+    description         = 'Minecraft Server Tools',
+    long_description    = readmeContent,
     author              = 'Monsieur Luge',
     author_email        = 'monsieurluge@gmail.com',
     url                 = 'https://github.com/MonsieurLuge/minecrafttools',
-    license             = license,
-    packages            = ['minecrafttools']
+    license             = licenseContent,
+    classifiers         = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords            = 'minecraft tools development',
+    packages            = ['minecrafttools'],
+    install_requires    = ['nbt'],
+    package_data        = {
+        'data': ['map colors.json']
+    }
 )
