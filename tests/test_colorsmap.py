e7103ab2777e39754615a520ab9ffc6f3fabc9ba
MonsieurLuge <monsieurluge@gmail.com>
2016-06-24 13:49:58 +0200

    colorsmap unit tests

--- tests/test_colorsmap.py
+++ tests/test_colorsmap.py
@@ -8,7 +8,7 @@ from minecrafttools.dimensions  import Dimensions # TODO MLG: use fake classes i
 
 class TestColorsMap(unittest.TestCase):
 
-    def test_Id(self):
+    def test_id(self):
         coordinates = Coordinates(1, 2)
         dimensions  = Dimensions(3, 4)
         colorsMap   = ColorsMap(
@@ -16,8 +16,41 @@ class TestColorsMap(unittest.TestCase):
             2,
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         )
+        # in this test case the id is 9
+        self.assertEqual(7, colorsMap.id(coordinates))
 
-        self.assertEqual(9, colorsMap.id(coordinates))
+    def test_height(self):
+        coordinates = Coordinates(1, 2)
+        dimensions  = Dimensions(3, 4)
+        colorsMap   = ColorsMap(
+            dimensions,
+            2,
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        )
+        # in this test case the height is 4
+        self.assertEqual(4, colorsMap.height())
+
+    def test_scale(self):
+        coordinates = Coordinates(1, 2)
+        dimensions  = Dimensions(3, 4)
+        colorsMap   = ColorsMap(
+            dimensions,
+            2,
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        )
+        # in this test case the scale is 2
+        self.assertEqual(2, colorsMap.scale())
+
+    def test_width(self):
+        coordinates = Coordinates(1, 2)
+        dimensions  = Dimensions(3, 4)
+        colorsMap   = ColorsMap(
+            dimensions,
+            2,
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        )
+        # in this test case the width is 3
+        self.assertEqual(3, colorsMap.width())
 
 if __name__ == '__main__':
     unittest.main()
