18cfc1aa2fab28f1d544314bdb753be644015b54
MonsieurLuge <monsieurluge@gmail.com>
2016-07-08 14:46:15 +0200

    add the MinecraftMapFile class

---
+++ minecrafttools/minecraftmapfile.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import nbt
+import os
+import re
+
+from minecrafttools.intcoordinates import IntCoordinates
+from minecrafttools.intdimensions  import IntDimensions
+from minecrafttools.map            import Map
+from nbt.nbt                       import NBTFile
+
+class MinecraftMapFile:
+
+    def __init__(self, path):
+        """ Creates a MinecraftMapFile object
+        Params:
+            path (string): the path to the map file on disk
+        """
+        self.__path = path
+
+    def content(self):
+        """ Returns the content of the file
+        Returns:
+            MinecraftMap
+        """
+        nbtData = NBTFile(self.__path, 'rb').get('data')
+
+        return Map(
+            self.name(),
+            IntDimensions(
+                str(nbtData.get('width')),
+                str(nbtData.get('height'))
+            ),
+            IntCoordinates(
+                str(nbtData.get('xCenter')),
+                str(nbtData.get('zCenter'))
+            ),
+            nbtData.get('colors')
+            self.lastModification()
+        )
+
+    def lastModification(self):
+        """ Returns the file last modification time (timestamp)
+        Returns:
+            integer
+        """
+        return os.path.getmtime(self.__path)
+
+    def name(self):
+        """ Returns the map name (ex: map_12)
+        Returns:
+            string
+        Raises:
+            Exception if the file is not properly named
+        """
+        found = re.search('(map_\d+).dat', self.__path);
+
+        if None == found:
+            raise Exception('the file ' + path + ' is not a regular map file (wrong name)')
+
+        return found.group(1)
