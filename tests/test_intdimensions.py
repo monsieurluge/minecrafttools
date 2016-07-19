2f0c043b63a9da429f72fc1621a52c6a3f084ace
MonsieurLuge <monsieurluge@gmail.com>
2016-07-19 17:25:49 +0200

    minor change : testSubject property

--- tests/test_intdimensions.py
+++ tests/test_intdimensions.py
@@ -7,15 +7,15 @@ from minecrafttools.intdimensions import IntDimensions
 class TestIntDimensions(unittest.TestCase):
 
     def setUp(self):
-        self.__dimensions = IntDimensions("10", 15.123)
+        self.testSubject = IntDimensions("10", 15.123)
 
     def test_width(self):
         # width is an integer value
-        self.assertEqual(10, self.__dimensions.width())
+        self.assertEqual(10, self.testSubject.width())
 
     def test_height(self):
         # height is an integer value
-        self.assertEqual(15, self.__dimensions.height())
+        self.assertEqual(15, self.testSubject.height())
 
 if __name__ == '__main__':
     unittest.main()
