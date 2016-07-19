4c3e70b494f66eb36c2a88cf13693a0714ea7836
MonsieurLuge <monsieurluge@gmail.com>
2016-07-19 17:25:41 +0200

    minor change : testSubject property

--- tests/test_intcoordinates.py
+++ tests/test_intcoordinates.py
@@ -7,15 +7,15 @@ from minecrafttools.intcoordinates import IntCoordinates
 class TestIntCoordinates(unittest.TestCase):
 
     def setUp(self):
-        self.__coordinates = IntCoordinates("10", 15.123)
+        self.testSubject = IntCoordinates("10", 15.123)
 
     def test_latitude(self):
         # latitude is an integer value
-        self.assertEqual((15), self.__coordinates.latitude())
+        self.assertEqual(15, self.testSubject.latitude())
 
     def test_longitude(self):
         # longitude is an integer value
-        self.assertEqual((10), self.__coordinates.longitude())
+        self.assertEqual(10, self.testSubject.longitude())
 
 if __name__ == '__main__':
     unittest.main()
