2528cd701dada50bfe8a6426ccc9b1586a236657
MonsieurLuge <monsieurluge@gmail.com>
2016-07-22 17:18:50 +0200

    doc block

--- minecrafttools/server.py
+++ minecrafttools/server.py
@@ -2,7 +2,6 @@
 
 from minecrafttools.properties import Properties
 from minecrafttools.whitelist  import Whitelist
-from minecrafttools.world      import World
 
 import os
 
@@ -16,19 +15,15 @@ class Server:
         self.__minecraftDirectory = minecraftDirectory
         self.__properties         = Properties(os.path.join(minecraftDirectory, 'server.properties'))
         self.__whitelist          = Whitelist(os.path.join(minecraftDirectory, 'whitelist.json'))
-        self.__world              = World(os.path.join(minecraftDirectory, self.__properties.valueOf('level-name')))
 
     def printWhitelist(self):
         """ Displays the players list """
         for player in self.__whitelist.playersSortedByName():
             print(player.id + ' /// ' + player.name)
 
-    def world(self):
-        """ Returns the World
-        Returns:
-            World
-        """
-        return self.__world
-
     def worldFolder(self):
+        """ Returns the active world folder
+        Returns:
+            string
+        """
         return os.path.join(self.__minecraftDirectory, self.__properties.valueOf('level-name'))
