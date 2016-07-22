870494df29f5047ee0d97613602f161c1a9de5ce
MonsieurLuge <monsieurluge@gmail.com>
2016-07-22 17:02:07 +0200

    refactoring

--- minecrafttools/scripts/cartographer.py
+++ minecrafttools/scripts/cartographer.py
@@ -14,52 +14,69 @@ from minecrafttools.server              import Server
 from minecrafttools.world               import World
 
 def generateCartography(minecraftDirectory, outputFolder, cartographyType):
-    ''' Generates the cartography
-
+    """ Generates the cartography
     Params:
         minecraftDirectory (string): the directory where the server.properties file is in
         outputDirectory (string):    the directory where to generate the cartography file(s)
         cartographyType (string):    what kind of cartography to generate (unique, multiple, [fragmented])
-    '''
+    """
     start = datetime.datetime.now()
 
     if cartographyType == 'multiple':
-        cartography = CartographyMultiple(
-            Maps(
-                World(
-                    Server(
-                        minecraftDirectory
-                    ).worldFolder()
-                ).mapsFolder()
-            ),
-            outputFolder
-        )
+        generateCartographyMultiple(minecraftDirectory, outputFolder)
     elif cartographyType == 'unique':
-        cartography = CartographyUnique(
-            Maps(
-                World(
-                    Server(
-                        minecraftDirectory
-                    ).worldFolder()
-                ).mapsFolder()
-            ),
-            outputFolder
-        )
+        generateCartographyUnique(minecraftDirectory, outputFolder)
     else:
         raise ValueError('"' + cartographyType + '" is not a valid cartography type')
-
-    try:
-        # Server(minecraftDirectory).world().cartography(cartographyType, outputDirectory).save()
-        cartography.save()
-    except (ValueError, IOError) as exception:
-        print('Error when trying to generate the cartography:', format(exception))
 
     end = datetime.datetime.now()
 
     print('time spent:', round((end - start) / datetime.timedelta(seconds=1), 2), 's')
 
+def generateCartographyMultiple(minecraftDirectory, outputFolder):
+    """ Generates the cartography 'multiple'
+    Params:
+        minecraftDirectory (string): the directory where the server.properties file is in
+        outputDirectory (string):    the directory where to generate the cartography file(s)
+    Raises:
+        ValueError, IOError
+    """
+    cartography = CartographyMultiple(
+        Maps(
+            World(
+                Server(
+                    minecraftDirectory
+                ).worldFolder()
+            ).mapsFolder()
+        ),
+        outputFolder
+    )
+
+    cartography.save()
+
+def generateCartographyUnique(minecraftDirectory, outputFolder):
+    """ Generates the cartography 'unique'
+    Params:
+        minecraftDirectory (string): the directory where the server.properties file is in
+        outputDirectory (string):    the directory where to generate the cartography file(s)
+    Raises:
+        ValueError, IOError
+    """
+    cartography = CartographyUnique(
+        Maps(
+            World(
+                Server(
+                    minecraftDirectory
+                ).worldFolder()
+            ).mapsFolder()
+        ),
+        outputFolder
+    )
+
+    cartography.save()
+
 def main(arguments):
-    ''' Main script for command-line purpose'''
+    """ Main script for command-line purpose """
     cartographyType    = 'multiple'
     minecraftDirectory = '~/.minecraft'
     outputDirectory    = '~/.minecraft/cartography'
@@ -93,8 +110,11 @@ def main(arguments):
         cartographyType = options.cartographytype
 
     # and generate the cartography
-    generateCartography(minecraftDirectory, outputDirectory, cartographyType)
+    try:
+        generateCartography(minecraftDirectory, outputDirectory, cartographyType)
+    except (ValueError, IOError):
+        print('Error when trying to generate the cartography (multiple):', format(exception))
 
-''' Launch the script / command-line usage'''
+""" Launch the script / command-line usage """
 if __name__ == "__main__":
     main(sys.argv[1:])
